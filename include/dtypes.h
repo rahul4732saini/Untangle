a1cdead624eda9c1d86434cffeff6cda2f71dc5e
rahul4732saini <rahul4732saini@gmail.com>
2025-09-04 12:48:42 +0530

    Initial commit

---
+++ include/dtypes.h
@@ -0,0 +1,22 @@
+#ifndef _DTYPES_H
+#define _DTYPES_H
+
+// Defines identifiers for different types of data which
+// can be directly parsed from the user input.
+
+// Primitive data types
+#define TYPE_BOOL 0
+#define TYPE_INT 1
+#define TYPE_FLOAT 2
+#define TYPE_COMPLEX 3
+#define TYPE_NUMERIC 4
+#define TYPE_STRING 5
+
+// Advanced data types
+#define TYPE_EQUATION 6
+#define TYPE_EXPRESSION 7
+#define TYPE_ARRAY 8
+#define TYPE_SET 9
+#define TYPE_MATRIX 10
+
+#endif
