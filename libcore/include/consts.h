f8142d543d0a69f4c4fb3a20e31d1652bca107ac
rahul4732saini <rahul4732saini@gmail.com>
2025-09-04 12:27:38 +0530

    Initial commit

---
+++ libcore/include/consts.h
@@ -0,0 +1,12 @@
+#ifndef _CONSTS_H
+#define _CONSTS_H
+
+#include <stdint.h>
+#include "plugin.h"
+
+extern const Strings fields;
+
+extern const Strings *domains[];
+extern const uint32_t domains_len;
+
+#endif
