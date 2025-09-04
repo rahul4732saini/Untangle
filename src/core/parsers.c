2befefb9b6b6b0b43855aed5371abf10dd8f4400
rahul4732saini <rahul4732saini@gmail.com>
2025-09-04 17:24:24 +0530

    Define 'parse_bool' function in parsers.c
    Defined the function to parse a boolean value from the specified source
    string.

--- src/core/parsers.c
+++ src/core/parsers.c
@@ -1,11 +1,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "consts.h"
 #include "typedefs.h"
 #include "structs.h"
+#include "tools.h"
 
 // Stores pointers to the data type parser functions.
 void *(*parsers[])(char *);
+
+bool *parse_bool(char *src)
+{
+    bool *result = (bool *)malloc(sizeof(bool));
+
+    if (str_case_cmp(src, bool_true))
+    {
+        *result = true;
+        return result;
+    }
+
+    else if (str_case_cmp(src, bool_false))
+    {
+        *result = false;
+        return result;
+    }
+
+    // Frees the memory and returns NULL if the source string does
+    // not represent an boolean value.
+    free(result);
+    return NULL;
+}
 
 integer_t *parse_int(char *src)
 {
