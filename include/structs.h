c799ae1513a488709be800d2068f38a95c0b7438
rahul4732saini <rahul4732saini@gmail.com>
2025-09-04 14:41:46 +0530

    Define shared structures in structs.h

---
+++ include/structs.h
@@ -0,0 +1,32 @@
+#ifndef _STRUCTS_H
+#define _STRUCTS_H
+
+#include "typedefs.h"
+
+typedef struct
+{
+    const char **strings;
+    len_t size;
+} Strings;
+
+typedef struct
+{
+    dtype_t type;
+    void *data;
+} ResultStep;
+
+typedef struct
+{
+    ResultStep *steps;
+    len_t size;
+} Result;
+
+typedef struct
+{
+    const dtype_t *args;
+    len_t args_size;
+    const Strings *prompts;
+    Result *(*handler)(void *, ...);
+} Problem;
+
+#endif
