4026db7a5073bbbf7bc7a6bdca4127dc11045caf
rahul4732saini <rahul4732saini@gmail.com>
2025-09-04 15:30:28 +0530

    Define 'parse_int' function in parsers.c
    Defined the function to parse an integer value from the specified source
    string.

--- src/core/parsers.c
+++ src/core/parsers.c
@@ -1,2 +1,23 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "typedefs.h"
+#include "structs.h"
+
 // Stores pointers to the data type parser functions.
 void *(*parsers[])(char *);
+
+integer_t *parse_int(char *src)
+{
+    integer_t *result = (integer_t *)malloc(sizeof(integer_t));
+
+    // Frees the memory and returns NULL if the source string does
+    // not represent an integer.
+    if (!sscanf(src, "%lld", result))
+    {
+        free(result);
+        return NULL;
+    }
+
+    return result;
+}
