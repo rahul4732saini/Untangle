2660faa51dc303d43fb146ee977493595c0eff26
rahul4732saini <rahul4732saini@gmail.com>
2025-09-06 12:16:30 +0530

    Define 'plugin_domain_func' variable in consts.c
    Defined the variable to store the name of the function within plugin
    to extract domain-related details. It is meant for locating the function
    within the plugin library.

--- src/consts.c
+++ src/consts.c
@@ -1,6 +1,10 @@
 // Relative path to the plugin directory.
 const char *plugin_dir = "plugin/";
 
+// Name of the function within the plugins to extract domain
+// related details, meant for locating it within the plugin.
+const char *plugin_domain_func = "get_domains";
+
 // String representation of boolean values expected from the user
 // for boolean inputs.
 const char *bool_true = "true";
