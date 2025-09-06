c04748a89214ef27003634f8ffadb6c84a0fc2f9
rahul4732saini <rahul4732saini@gmail.com>
2025-09-06 12:47:54 +0530

    Define 'get_plugins' function in posix.c
    Defined the function to load the plugin libraries into memory and also
    save pointers to the target functions.

--- src/arch/posix.c
+++ src/arch/posix.c
@@ -78,3 +78,70 @@ static len_t get_plugin_count(char *path)
     closedir(dir);
     return ctr;
 }
+
+/**
+ * @brief Extracts the domains extracter functions from the plugin libraries.
+ */
+Domains *(**get_plugins(void))(void)
+{
+    // Avoid re-loading the libraries and extracting the functions if
+    // already done.
+    if (handlers)
+        return &plugins;
+
+    char *dir_path = get_plugin_dir();
+
+    len_t file_cnt = get_plugin_count(dir_path);
+    len_t dir_path_len = strlen(dir_path);
+
+    handlers = malloc(file_cnt * sizeof(void *));
+    plugins.functions = ((Domain * (**)(void)) malloc(file_cnt * sizeof(Domain * (*)(void))));
+
+    // Keeps track of the current index in the handlers and functions array.
+    len_t ctr = 0;
+
+    DIR *dir = opendir(dir_path);
+    struct dirent *entry;
+
+    while ((entry = readdir(dir)))
+    {
+        if (entry->d_type != DT_REG)
+            continue;
+
+        char *match = strstr(entry->d_name, lib_file_suffix);
+
+        if (!match || match[lib_file_suffix_len])
+            continue;
+
+        // Extracts and saves the absolute path to the plugin library to avoid
+        // loading a library from the global environment if the name conflicts.
+        char buff[dir_path_len + strlen(entry->d_name) + 1];
+        strcpy(buff, dir_path);
+        strcat(buff, entry->d_name);
+
+        void *handler = dlopen(buff, lib_open_mode);
+
+        // Continues if the library cannot be opened successfully.
+        if (!handler)
+            continue;
+
+        Domains *(*function)(void) = dlsym(handler, plugin_domain_func);
+
+        // Continues if the target function was not found in the library.
+        if (!function)
+        {
+            dlclose(handler);
+            continue;
+        }
+
+        // Stores the handler and the function, and updates the counter.
+        handlers[ctr] = handler;
+        plugins.functions[ctr++] = function;
+    }
+
+    // Saves the counter for future usage.
+    plugins.size = plugin_count = ctr;
+
+    free(dir_path);
+    return &plugins;
+}
