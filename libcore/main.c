832a4e0c4ec7cf3751c11f2a4965737cbfedb5f3
rahul4732saini <rahul4732saini@gmail.com>
2025-09-06 07:35:32 +0530

    Define 'plugin_name' variable in main.c
    Defined the variable to store the name of the plugin to be displayed
    within the application interface.

--- libcore/main.c
+++ libcore/main.c
@@ -1,6 +1,9 @@
 #include "structs.h"
 #include "topics.h"
 
+// Name of the plugin to be displayed within the application.
+const char *plugin_name = "Core Plugin";
+
 const Domains *get_domains(void)
 {
     return &domains;
