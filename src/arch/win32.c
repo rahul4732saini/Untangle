3ad8e219f1dc111e40df59d2640d288272786809
rahul4732saini <rahul4732saini@gmail.com>
2025-09-06 12:26:11 +0530

    Define 'get_plugin_count' function in win32.c
    Defined the function to extract the number of plugin files present
    within the specified directory.

--- src/arch/win32.c
+++ src/arch/win32.c
@@ -20,3 +20,36 @@ static char *get_plugin_dir(void)
     sprintf(buff + length, "\\%s\\", plugin_dir);
     return buff;
 }
+
+/**
+ * @brief Extracts the number of plugin files within the specified directory.
+ */
+static len_t get_plugin_count(char *path)
+{
+    // Creates a separate buffer to store the directory path and the
+    // pattern specification to include all the file and directories.
+    char buff[strlen(path) + 1];
+    sprintf(buff, "%s*", path);
+
+    WIN32_FIND_DATA fs_entry;
+    HANDLE hFind = FindFirstFile(buff, &fs_entry);
+
+    len_t ctr = 0;
+
+    do
+    {
+        if (fs_entry.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            continue;
+
+        char *match = strstr(fs_entry.cFileName, lib_file_suffix);
+
+        // Skips if no match is found or there are more characters
+        // present ahead of the expected suffix.
+        if (match && !match[lib_file_suffix_len])
+            ++ctr;
+
+    } while (FindNextFile(hFind, &fs_entry));
+
+    FindClose(hFind);
+    return ctr;
+}
