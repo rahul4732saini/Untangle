e91dcdd668c281a42f5bc92067d2383011eddf84
rahul4732saini <rahul4732saini@gmail.com>
2025-09-06 07:56:12 +0530

    Define 'plugin_dir' variable in consts.c
    Defined the variable to store the relative path to the plugin directory
    within the application directory.

--- src/consts.c
+++ src/consts.c
@@ -1,3 +1,6 @@
+// Relative path to the plugin directory.
+const char *plugin_dir = "plugin/";
+
 // String representation of boolean values expected from the user
 // for boolean inputs.
 const char *bool_true = "true";
