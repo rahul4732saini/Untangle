a66c2444a11576648d2f558c2c77af64bbfc6a8f
rahul4732saini <rahul4732saini@gmail.com>
2025-09-06 14:38:45 +0530

    Define 'str_strip' function in tools.c
    Defined the function to strip out leading and trailing spaces from the
    specified stirng.

--- common/tools.c
+++ common/tools.c
@@ -1,5 +1,8 @@
 #include <stdbool.h>
 #include <ctype.h>
+#include <string.h>
+
+#include "typedefs.h"
 
 /**
  * @brief Compares the given two strings irrespective of their character casing.
@@ -11,4 +14,23 @@ bool str_case_cmp(char *str1, char *str2)
             break;
 
     return !*str1 && !*str2;
+}
+
+/**
+ * @brief Strips leading and trailing spaces from the string.
+ */
+char *str_strip(char *src)
+{
+    while (*src == ' ')
+        ++src;
+
+    for (len_t i = strlen(src) - 1; i >= 0; --i)
+    {
+        if (src[i] != ' ')
+            break;
+
+        src[i] = 0;
+    }
+
+    return src;
 }
