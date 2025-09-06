3668bb29d2a39bab91a45d5f9770fbe84dee1a0c
rahul4732saini <rahul4732saini@gmail.com>
2025-09-06 15:46:33 +0530

    Define 'is_valid_plugin_file' function in win32.c
    Defined the function to check whether the specified directory entry
    marks a valid plugin file.

--- src/arch/win32.c
+++ src/arch/win32.c
@@ -1,12 +1,29 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <windows.h>
 
 #include "consts.h"
 #include "typedefs.h"
+#include "structs.h"
 
 static const char *lib_file_suffix = ".dll";
 static const len_t lib_file_suffix_len = 4;
+
+/**
+ * @brief Validates the specified directory entry for a plugin file.
+ */
+static bool is_valid_plugin_file(WIN32_FIND_DATA *fs_entry)
+{
+    if (fs_entry->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+        return false;
+
+    char *match = strstr(fs_entry->cFileName, lib_file_suffix);
+
+    // Only returns true if a match was found and there are no
+    // characters present ahead of the expected suffix.
+    return match && !match[lib_file_suffix_len];
+}
 
 /**
  * @brief Extracts the absolute path to the plugin directory.
