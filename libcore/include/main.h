31a6db134ebfd62858d6a35bd461776f9847b97a
rahul4732saini <rahul4732saini@gmail.com>
2025-09-06 07:36:34 +0530

    Update function declarations in main.h
    Updated function signatures, and removed redundant functions
    based on the changes made in the associated C source code file.

--- libcore/include/main.h
+++ libcore/include/main.h
@@ -1,10 +1,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-#include <stdint.h>
 #include "structs.h"
 
-extern const Strings *get_fields(void);
-extern const Strings *get_domains(uint32_t inx);
+extern const Domains *get_domains(void);
 
 #endif
