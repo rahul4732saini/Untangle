654bfea8f02a0cb07e151621fe145406e61e7041
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 15:42:39 +0530

    Define 'show_footer' function in menu.c
    Defined the function to display the footer string comrpsing general
    details about the software at the bottom of the screen.

---
+++ src/interface/tui/screens/menu.c
@@ -0,0 +1,25 @@
+#include <ncurses.h>
+#include <string.h>
+
+#include "typedefs.h"
+
+#include "interface/tui/consts.h"
+#include "interface/tui/typedefs.h"
+
+/**
+ * @brief Displays the footer strings comprising general details about
+ * the software.
+ *
+ * @param scr_dim Pointer to the Dimension struct comprising the
+ * screen dimensions.
+ */
+void show_footer(Dimension *scr_dim)
+{
+    index_t row = scr_dim->height - footers_len - 1;
+
+    for (index_t i = 0; i < footers_len; ++i, ++row)
+    {
+        move(row, (scr_dim->width - strlen(footers[i])) / 2);
+        printw("%s", footers[i]);
+    }
+}
