1fd748211f0cc2dad663e100fc34b3bd70b8d64b
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 14:04:26 +0530

    Define types in typedefs.h

---
+++ src/include/interface/tui/typedefs.h
@@ -0,0 +1,24 @@
+#ifndef _TUI_STRUCTS_H
+#define _TUI_STRUCTS_H
+
+#include <ncurses.h>
+#include <stdint.h>
+
+typedef uint16_t pos_t;
+typedef uint16_t span_t;
+
+typedef struct
+{
+    WINDOW *win;
+    Dimension *dim;
+} WinContext;
+
+typedef struct
+{
+    span_t height;
+    span_t width;
+    pos_t start_y;
+    pos_t start_x;
+} Dimension;
+
+#endif
