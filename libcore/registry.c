371df32da1a0e972f8b72d4853173b6c199e5823
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 11:06:43 +0530

    Define 'plugin_name' variable in registry.c
    Defined the variable to store the name of the plugin to be displayed in
    the application interface.

--- libcore/registry.c
+++ libcore/registry.c
@@ -5,3 +5,8 @@
 #define EXPORT __attribute__((visibility("default")))
 
 #endif
+
+#include "structs.h"
+
+// Name of the plugin to be displayed in the application interface.
+EXPORT const char *plugin_name = "Core Plugin";
