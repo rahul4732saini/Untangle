7bd4622883631b6d5e7e894c33b0581951ad7946
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 11:13:50 +0530

    Remove redundant source code files

--- libcore/include/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#include "structs.h"
-
-extern const char *plugin_name;
-extern const Domains *get_domains(void);
-
-#endif
--- libcore/include/topics.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TOPICS_H
-#define _TOPICS_H
-
-extern const Domain domains;
-
-#endif
