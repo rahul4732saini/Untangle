897fa7b0b498ab506b592e7e03c29d0f2e458e6b
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 11:18:39 +0530

    Refactor constants in consts.c
    Renamed constant 'plugin_domain_func' to 'plugin_domains_var' to store
    the name of the variable storing the domains rather than the function
    for extracting them based on the changes made in the plugin structure.
    Also updated the associated comments to ensure clarity.

--- src/consts.c
+++ src/consts.c
@@ -1,9 +1,9 @@
 // Relative path to the plugin directory.
 const char *plugin_dir = "plugin";
 
-// Name of the variables and functions declared by plugin libraries,
-// used for locating the associated symbols in the plugin.
-const char *plugin_domain_func = "get_domains";
+// Name of the variables declared by plugin libraries, used for
+// locating the associated symbols in the plugin.
+const char *plugin_domains_var = "domains";
 const char *plugin_name_var = "plugin_name";
 
 // String representation of boolean values expected from the user
