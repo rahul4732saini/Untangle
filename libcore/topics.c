f5236d09a3e47d9c70a9c601fc24f69b51eb9d66
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 08:33:44 +0530

    Define Field structs in topics.c
    Defines sturctures for storing mathematical fields, and updated the
    'Domain' struct for mathematics to store pointers to these fields.

--- libcore/topics.c
+++ libcore/topics.c
@@ -9,12 +9,42 @@
 
 #include "structs.h"
 
-Domain dmn_math = {
-    .name = "Mathematics",
-    .fields_size = 0,
+const Field fld_basic_algebra = {
+    .name = "Elementary Algebra",
+    .problems_size = 0,
 };
 
-Domain *domains_arr[] = {
+const Field fld_matrix_manipulation = {
+    .name = "Matrix Manipulation",
+    .problems_size = 0,
+};
+
+const Field fld_arithmetic_progression = {
+    .name = "Arithmetic Progressions",
+    .problems_size = 0,
+};
+
+const Field fld_geometric_progression = {
+    .name = "Geometric Progressions",
+    .problems_size = 0,
+};
+
+const Field *fields_math_arr[] = {
+    &fld_basic_algebra,
+    &fld_matrix_manipulation,
+    &fld_arithmetic_progression,
+    &fld_geometric_progression,
+};
+
+const len_t fields_mat_arr_len = 4;
+
+const Domain dmn_math = {
+    .name = "Mathematics",
+    .fields = fields_math_arr,
+    .fields_size = 4,
+};
+
+const Domain *domains_arr[] = {
     &dmn_math,
 };
 
