963d6ca748f579743c208ae9a521a817bb2cc7c6
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 06:57:51 +0530

    Define constant 'plugin_name_var' in consts.c
    Defined the constant to store the name of the variable storing the name
    of the plugin meant to be dispalyed within the application interface.

--- src/consts.c
+++ src/consts.c
@@ -1,9 +1,10 @@
 // Relative path to the plugin directory.
 const char *plugin_dir = "plugin";
 
-// Name of the function within the plugins to extract domain
-// related details, meant for locating it within the plugin.
+// Name of the variables and functions declared by plugin libraries,
+// used for locating the associated symbols in the plugin.
 const char *plugin_domain_func = "get_domains";
+const char *plugin_name_var = "plugin_name";
 
 // String representation of boolean values expected from the user
 // for boolean inputs.
