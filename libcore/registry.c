7ca16c84bf3a587b0cdae760bee442b77fe7d1d8
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 11:09:57 +0530

    Define constants in registry.c
    Defined constant variables to store the different fields and domains
    supported by the plugin library.

--- libcore/registry.c
+++ libcore/registry.c
@@ -10,3 +10,50 @@
 
 // Name of the plugin to be displayed in the application interface.
 EXPORT const char *plugin_name = "Core Plugin";
+
+const Field fld_basic_algebra = {
+    .name = "Elementary Algebra",
+    .problems_size = 0,
+};
+
+const Field fld_matrix_manipulation = {
+    .name = "Matrix Manipulation",
+    .problems_size = 0,
+};
+
+const Field fld_arithmetic_progression = {
+    .name = "Arithmetic Progressions",
+    .problems_size = 0,
+};
+
+const Field fld_geometric_progression = {
+    .name = "Geometric Progressions",
+    .problems_size = 0,
+};
+
+const Field *fields_math_arr[] = {
+    &fld_basic_algebra,
+    &fld_matrix_manipulation,
+    &fld_arithmetic_progression,
+    &fld_geometric_progression,
+};
+
+const len_t fields_mat_arr_len = 4;
+
+const Domain dmn_math = {
+    .name = "Mathematics",
+    .fields = fields_math_arr,
+    .fields_size = 4,
+};
+
+const Domain *domains_arr[] = {
+    &dmn_math,
+};
+
+const len_t domains_arr_len = 1;
+
+// Hierarchy of domains, fields, and problems for users to choose.
+EXPORT const Domains domains = {
+    .domains = domains_arr,
+    .domains_size = domains_arr_len,
+};
