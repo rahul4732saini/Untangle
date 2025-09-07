6b4a096e6622da77796e9f3ea8bbd45f2471498e
rahul4732saini <rahul4732saini@gmail.com>
2025-09-07 13:12:18 +0530

    Update parsers functions in parsers.c
    Updated the functions to return a generic 'void *' rather than pointers
    to specific data types to allow them to be stored in the 'parsers' array.

--- src/core/parsers.c
+++ src/core/parsers.c
@@ -25,7 +25,7 @@ void *(*parsers[])(char *) = {
     parse_string,
 };
 
-static bool *parse_bool(char *src)
+static void *parse_bool(char *src)
 {
     bool *result = (bool *)malloc(sizeof(bool));
 
@@ -47,7 +47,7 @@ static bool *parse_bool(char *src)
     return NULL;
 }
 
-static integer_t *parse_int(char *src)
+static void *parse_int(char *src)
 {
     integer_t *result = (integer_t *)malloc(sizeof(integer_t));
     len_t ctr = 0;
@@ -63,7 +63,7 @@ static integer_t *parse_int(char *src)
     return NULL;
 }
 
-static double *parse_float(char *src)
+static void *parse_float(char *src)
 {
     double *result = (double *)malloc(sizeof(double));
     len_t ctr = 0;
@@ -79,7 +79,7 @@ static double *parse_float(char *src)
     return NULL;
 }
 
-static Number *parse_complex(char *src)
+static void *parse_complex(char *src)
 {
     Number *result = (Number *)malloc(sizeof(Number));
     result->real = 0;
@@ -120,7 +120,7 @@ fail:
     return NULL;
 }
 
-static Number *parse_numeric(char *src)
+static void *parse_numeric(char *src)
 {
     Number *result = (Number *)malloc(sizeof(Number));
     result->imag = 0;
@@ -154,7 +154,7 @@ static Number *parse_numeric(char *src)
     return result;
 }
 
-static char *parse_string(char *src)
+static void *parse_string(char *src)
 {
     return src;
 }
