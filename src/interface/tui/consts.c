838f126b5d41a39f453fe9cbb9c5c129901a99bb
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 12:12:28 +0530

    Define 'search_box_start_y' constant in consts.c
    Defined the constant to store the starting Y coordinate of the search
    box in all Sub Menu sections.

--- src/interface/tui/consts.c
+++ src/interface/tui/consts.c
@@ -5,8 +5,9 @@ const span_t main_menu_width = 24; // Also includes the window's side borders.
 
 const char *go_back_button_text = "Go Back [Esc]";
 
-// Starting coordinates of the 'Go Back' button on the TUI screen.
+// Starting coordinates of different components on the TUI screen.
 const pos_t go_back_button_start_y = title_bar_height;
 const pos_t go_back_button_start_x = 1;
+const pos_t search_box_start_y = go_back_button_start_y + 1;
 
 const char *domain_menu_title = "Select a Domain";
