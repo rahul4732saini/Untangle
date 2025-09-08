6bceae377f7dff2af60a6eab64518461082bb658
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 08:21:54 +0530

    Update 'main_menu_width' constant in consts.c
    Increased the width of the main menu for a better visual appeal.

--- src/interface/tui/consts.c
+++ src/interface/tui/consts.c
@@ -1,3 +1,3 @@
 #include "interface/tui/typedefs.h"
 
-const span_t main_menu_width = 20; // Also includes the window's side borders.
+const span_t main_menu_width = 24; // Also includes the window's side borders.
