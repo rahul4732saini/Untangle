4ec3ef441ff6988cd81c544924d18f57308b79c0
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 12:03:24 +0530

    Declare 'show_domain_menu_screen' function in sub_menu.h

--- src/include/interface/tui/screens/sub_menus.h
+++ src/include/interface/tui/screens/sub_menus.h
@@ -2,7 +2,9 @@
 #define _TUI_SCR_SUB_MENUS_H
 
 #include <stdbool.h>
+#include "interface/tui/typedefs.h"
 
 extern void show_go_back_button(bool selected);
+extern void show_domain_menu_screen(Dimension *scr_dim);
 
 #endif
