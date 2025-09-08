8ffc7cac0b3c3e7360e635c5d186dddd97fc315c
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 10:10:50 +0530

    Add msising 'const' qualifier in variables in consts.h

--- src/include/consts.h
+++ src/include/consts.h
@@ -3,20 +3,20 @@
 
 #include "typedefs.h"
 
-extern char *plugin_dir;
-extern char *plugin_domains_var;
-extern char *plugin_name_var;
+extern const char *plugin_dir;
+extern const char *plugin_domains_var;
+extern const char *plugin_name_var;
 
-extern char *bool_true;
-extern char *bool_false;
+extern const char *bool_true;
+extern const char *bool_false;
 
-extern char *main_menu_items[];
-extern char *footers[];
+extern const char *main_menu_items[];
+extern const char *footers[];
 
-extern len_t main_menu_items_len;
-extern len_t footers_len;
+extern const len_t main_menu_items_len;
+extern const len_t footers_len;
 
-extern char *main_menu_top_bar_title;
-extern char *main_menu_title;
+extern const char *main_menu_top_bar_title;
+extern const char *main_menu_title;
 
 #endif
