5cbd87c43696cf562a8f5c210ad471c0df7b3b12
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 07:38:19 +0530

    Declare 'main_menu_width' variable in consts.h

--- src/include/interface/tui/consts.h
+++ src/include/interface/tui/consts.h
@@ -1,9 +1,8 @@
 #ifndef _TUI_CONSTS_H
 #define _TUI_CONSTS_H
 
-#include "typedefs.h"
+#include "interface/tui/typedefs.h"
 
-extern char *footers[];
-extern len_t footers_len;
+extern span_t main_menu_width;
 
 #endif
