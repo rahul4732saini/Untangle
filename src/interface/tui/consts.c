1bf5363d11ed248aa7106225dcb30361eb9a5c79
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 11:49:20 +0530

    Define additional constants in consts.c
    Defined constants related with the 'Go Back' button to be displayed
    within sub menus.

--- src/interface/tui/consts.c
+++ src/interface/tui/consts.c
@@ -2,3 +2,9 @@
 
 const span_t title_bar_height = 2;
 const span_t main_menu_width = 24; // Also includes the window's side borders.
+
+const char *go_back_button_text = "Go Back [Esc]";
+
+// Starting coordinates of the 'Go Back' button on the TUI screen.
+const pos_t go_back_button_start_y = title_bar_height;
+const pos_t go_back_button_start_x = 1;
