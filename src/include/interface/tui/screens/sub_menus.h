eec489bd5ecd0bf1135a08901ad8d179b11180a6
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 16:43:29 +0530

    Update function signatures in sub_menus.h

--- src/include/interface/tui/screens/sub_menus.h
+++ src/include/interface/tui/screens/sub_menus.h
@@ -5,7 +5,7 @@
 #include "interface/tui/typedefs.h"
 
 extern void show_go_back_button(bool selected);
-extern void show_domain_menu_screen(Dimension *scr_dim);
+extern void show_sub_menu_screen(char *title, Dimension *scr_dim);
 extern void init_search_window(WinContext *wctx, Dimension *scr_dim);
 extern void show_search_window(WinContext *wctx, char *text, bool selected);
 extern void init_sub_menu_window(WinContext *wctx, Dimension *scr_dim);
