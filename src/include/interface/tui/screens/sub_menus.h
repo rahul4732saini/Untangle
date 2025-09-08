fd6c94e8f3197d70d8b8d4b5510c73c8bdef29c3
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 15:42:40 +0530

    Declare 'init_sub_menu_window' function in sub_menus.h

--- src/include/interface/tui/screens/sub_menus.h
+++ src/include/interface/tui/screens/sub_menus.h
@@ -8,5 +8,6 @@ extern void show_go_back_button(bool selected);
 extern void show_domain_menu_screen(Dimension *scr_dim);
 extern void init_search_window(WinContext *wctx, Dimension *scr_dim);
 extern void show_search_window(WinContext *wctx, char *text, bool selected);
+extern void init_sub_menu_window(WinContext *wctx, Dimension *scr_dim);
 
 #endif
