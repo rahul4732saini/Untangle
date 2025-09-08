8a59feb5137cde5334cbac1efeee46a2e489978c
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 12:04:50 +0530

    Define 'domain_menu_title' constant in consts.c
    Defined the constant variable to store the title to be displayed in the
    title bar in the Domain Menu.

--- src/interface/tui/consts.c
+++ src/interface/tui/consts.c
@@ -8,3 +8,5 @@ const char *go_back_button_text = "Go Back [Esc]";
 // Starting coordinates of the 'Go Back' button on the TUI screen.
 const pos_t go_back_button_start_y = title_bar_height;
 const pos_t go_back_button_start_x = 1;
+
+const char *domain_menu_title = "Select a Domain";
