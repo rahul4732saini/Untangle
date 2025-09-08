21881396176e611bd0d880419c797b55500ee6fb
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 07:35:13 +0530

    Add constant variable declarations in consts.h

--- src/include/consts.h
+++ src/include/consts.h
@@ -1,11 +1,22 @@
 #ifndef _CONSTS_H
 #define _CONSTS_H
 
+#include "typedefs.h"
+
 extern char *plugin_dir;
 extern char *plugin_domains_var;
 extern char *plugin_name_var;
 
 extern char *bool_true;
 extern char *bool_false;
+
+extern char *main_menu_items[];
+extern char *footers[];
+
+extern len_t main_menu_items_len;
+extern len_t footers_len;
+
+extern char *main_menu_top_bar_title;
+extern char *main_menu_title;
 
 #endif
