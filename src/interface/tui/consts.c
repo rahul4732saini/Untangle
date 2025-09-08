ac1fb73a2f6ecb7623d70d3ef2ed3e61dca1f6f3
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 11:48:32 +0530

    Remove redundant constant definitions in consts.c

--- src/interface/tui/consts.c
+++ src/interface/tui/consts.c
@@ -2,7 +2,3 @@
 
 const span_t title_bar_height = 2;
 const span_t main_menu_width = 24; // Also includes the window's side borders.
-
-// Text to be displayed in the search button along with the search
-// box in the search window.
-const char *search_button_text = "Search [Enter]";
