f1a7b33605c284d47193b5e802b713d8924c5442
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 10:28:03 +0530

    Declare 'show_search_button' in search.h

---
+++ src/include/interface/tui/screens/search.h
@@ -0,0 +1,9 @@
+#ifndef _TUI_SCR_SEARCH_H
+#define _TUI_SCR_SEARCH_H
+
+#include <stdbool.h>
+#include "interface/tui/typedefs.h"
+
+void show_search_button(pos_t start_y, Dimension *scr_dim, bool selected);
+
+#endif
