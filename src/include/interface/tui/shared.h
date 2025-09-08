651839a04ab1defae1a43dda1afd494f9cada148
rahul4732saini <rahul4732saini@gmail.com>
2025-09-08 07:37:54 +0530

    Add function declarations in shared.h

---
+++ src/include/interface/tui/shared.h
@@ -0,0 +1,10 @@
+#ifndef _TUI_SHARED_H
+#define _TUI_SHARED_H
+
+#include <ncurses.h>
+#include "interface/tui/typedefs.h"
+
+extern WINDOW *init_window(Dimension *dim);
+extern void show_title_bar(char *heading, Dimension *scr_dim);
+
+#endif
