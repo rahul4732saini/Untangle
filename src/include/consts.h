1ba5d305bd809ce30a331d1a71913c0e0ad59537
rahul4732saini <rahul4732saini@gmail.com>
2025-09-09 12:33:17 +0530

    Rename constants in consts.h

--- src/include/consts.h
+++ src/include/consts.h
@@ -16,7 +16,7 @@ extern const char *footers[];
 extern const len_t main_menu_items_len;
 extern const len_t footers_len;
 
-extern const char *main_menu_top_bar_title;
 extern const char *main_menu_title;
+extern const char *main_menu_window_title;
 
 #endif
