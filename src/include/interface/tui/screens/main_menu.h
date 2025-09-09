1480f2cd0816f1b66fcf5980584b4e9284ba0c62
rahul4732saini <rahul4732saini@gmail.com>
2025-09-10 05:20:11 +0530

    Declare 'setup_main_menu_screen' function in main_menu.h

--- src/include/interface/tui/screens/main_menu.h
+++ src/include/interface/tui/screens/main_menu.h
@@ -4,6 +4,7 @@
 #include "typedefs.h"
 #include "interface/tui/typedefs.h"
 
+extern void setup_main_menu_screen(Dimension *scr_dim);
 extern void init_main_menu(WinContext *wctx, Dimension *scr_dim);
 extern void show_main_menu(WinContext *wctx, index_t select);
 
