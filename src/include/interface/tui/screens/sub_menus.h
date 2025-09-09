af7e2eda3c4d730e3ccedef75528ddd5fab2962b
rahul4732saini <rahul4732saini@gmail.com>
2025-09-09 10:24:50 +0530

    Add function declarations in sub_menus.h

--- src/include/interface/tui/screens/sub_menus.h
+++ src/include/interface/tui/screens/sub_menus.h
@@ -2,12 +2,21 @@
 #define _TUI_SCR_SUB_MENUS_H
 
 #include <stdbool.h>
+
+#include "typedefs.h"
+#include "structs.h"
 #include "interface/tui/typedefs.h"
 
 extern void show_go_back_button(bool selected);
-extern void show_sub_menu_screen(const char *title, Dimension *scr_dim);
-extern void init_search_window(WinContext *wctx, Dimension *scr_dim);
 extern void show_search_window(WinContext *wctx, char *text, bool selected);
+
+extern void show_sub_menu_screen(const char *title, Dimension *scr_dim);
+
+extern void init_search_window(WinContext *wctx, Dimension *scr_dim);
 extern void init_sub_menu_window(WinContext *wctx, Dimension *scr_dim);
+
+extern void show_domain_menu_window(WinContext *wctx, Domains *domains, index_t select);
+extern void show_field_menu_window(WinContext *wctx, Domain *domain, index_t select);
+extern void show_problem_menu_window(WinContext *wctx, Field *domain, index_t select);
 
 #endif
