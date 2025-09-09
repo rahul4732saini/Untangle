9c7e6aea2ad23eeb4969c111979dcbcb5a570c97
rahul4732saini <rahul4732saini@gmail.com>
2025-09-09 13:53:47 +0530

    Define screen handler related macros in handlers.h
    Defined macros for storing the identifier for each screen handler
    function which will be used by them, to jump from one to another.

--- src/include/interface/tui/handlers.h
+++ src/include/interface/tui/handlers.h
@@ -1,4 +1,11 @@
 #ifndef _TUI_HANDLERS_H
 #define _TUI_HANDLERS_H
 
+#define HDL_PREV -1
+#define HDL_MAIN_MENU 0
+#define HDL_EXTENSION_MENU 4
+#define HDL_DOMAIN_MENU 1
+#define HDL_FIELD_MENU 2
+#define HDL_PROBLEM_MENU 3
+
 #endif
