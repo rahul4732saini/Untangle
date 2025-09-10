f30f5774d66e133ab4dd01580ec0f910d008fd79
rahul4732saini <rahul4732saini@gmail.com>
2025-09-10 15:26:28 +0530

    Initial commit

---
+++ src/include/hash.h
@@ -0,0 +1,12 @@
+#ifndef _HASH_H
+#define _HASH_H
+
+#include "typedefs.h"
+#include "shared.h"
+
+HashTable *init_hash_table(len_t size, char *(*)(void *));
+void *add_table_data(HashTable *table, void *data);
+void *get_table_data(HashTable *table, char *key);
+void free_hash_table(HashTable *table);
+
+#endif
