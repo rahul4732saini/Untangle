8dbc3fdde3d26e490b3d6a77a9df7142d20d7d13
rahul4732saini <rahul4732saini@gmail.com>
2025-09-27 19:51:12 +0530

    Rename 'arch.h' to 'platform.h'
    Renamed the file to better signify the functionalities provided by it.

--- src/include/platform.h
+++ src/include/platform.h
@@ -1,8 +1,8 @@
 // Common header file providing declarations for all the
 // architecture-dependent functionalities.
 
-#ifndef _ARCH_H
-#define _ARCH_H
+#ifndef _PLATFORM_H
+#define _PLATFORM_H
 
 #include "shared.h"
 
