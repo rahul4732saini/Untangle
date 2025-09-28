52fdd2ac3340737ac5f8309376150bbffc04837d
rahul4732saini <rahul4732saini@gmail.com>
2025-09-28 09:51:23 +0530

    Update functiobn signatures in session.h
    Updated the signatures based on the changes made in the associated
    functions in the source code file.

--- src/include/session.h
+++ src/include/session.h
@@ -1,10 +1,13 @@
 #ifndef _SESSION_H
 #define _SESSION_H
 
+#include <stdbool.h>
+
+#include "typedefs.h"
 #include "shared.h"
 
 SessionData *get_session_data(void);
-void toggle_plugin(PluginData *plugin);
+bool switch_plugin(index_t inx);
 void clean_session_data(void);
 
 #endif
