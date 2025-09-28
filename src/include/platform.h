36ed26b6384ac3463cb190d056b79b26a2e5c478
rahul4732saini <rahul4732saini@gmail.com>
2025-09-28 09:50:57 +0530

    Declare 'toggle_plugin' function in platform.h

--- src/include/platform.h
+++ src/include/platform.h
@@ -4,9 +4,13 @@
 #ifndef _PLATFORM_H
 #define _PLATFORM_H
 
+#include <stdbool.h>
+
+#include "typedefs.h"
 #include "shared.h"
 
 extern PluginsData *get_plugins(void);
+extern bool toggle_plugin(index_t inx);
 extern void clean_plugins(void);
 
 #endif
